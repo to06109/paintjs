497aede8bf60f20a41c5a9b78724861e56a4d3d2
to06109 <69625013+to06109@users.noreply.github.com>
2022-03-05 00:38:29 +0900

    220304 implementation change color

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const canvas = document.getElementById('jsCanvas')
 // canvas는 context를 가짐-> context: canvas 요소 안에서 픽셀에 접근할 수 있는 방법
 const ctx = canvas.getContext('2d')
+const colors = document.getElementsByClassName('jsColor')
 
 // 캔버스는 두 개의 사이즈가 지정되어야 함-> css 사이즈, element 사이즈
 // 캔버스를 픽셀을 다룰 수 있는 element로서 만드는거니까 element에 width와 height를 지정해줘야함
+// 픽셀을 다루는 윈도우가 얼마나 큰지 canvas에게 알려줌
 canvas.width = 700
 canvas.height = 700
 
@@ -31,13 +33,15 @@ function onMouseMove(event) {
     ctx.beginPath()
     ctx.moveTo(x, y)
   } else {
-    ctx.lineTo(x, y) // (x,y 지점까지 선을 이어줌)
+    ctx.lineTo(x, y) // path의 이전 위치에서 현재 위치까지 선을 그음(x,y 지점까지 선을 이어줌)
     ctx.stroke() //눈에 보이게 선을 그음
   }
 }
 
-function onMouseDown(event) {
-  painting = true
+function handleColorClick(event) {
+  // console.log(event.target.style)
+  const color = event.target.style.backgroundColor
+  ctx.strokeStyle = color
 }
 
 if (canvas) {
@@ -49,3 +53,7 @@ if (canvas) {
   // 캔버스에서 벗어나는 순간을 인지
   canvas.addEventListener('mouseleave', stopPainting)
 }
+
+Array.from(colors).forEach((color) =>
+  color.addEventListener('click', handleColorClick),
+)
