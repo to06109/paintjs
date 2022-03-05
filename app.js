076e2cb81da4bb1f4526163eec0d6c92696f5a1b
to06109 <69625013+to06109@users.noreply.github.com>
2022-03-05 16:41:59 +0900

    complete implementation paintboard

--- app.js
+++ app.js
@@ -2,17 +2,27 @@ const canvas = document.getElementById('jsCanvas')
 // canvas는 context를 가짐-> context: canvas 요소 안에서 픽셀에 접근할 수 있는 방법
 const ctx = canvas.getContext('2d')
 const colors = document.getElementsByClassName('jsColor')
+const range = document.getElementById('jsRange') // brush size 버튼
+const mode = document.getElementById('jsMode') // fill 버튼
+const saveBtn = document.getElementById('jsSave') // save 버튼
+
+const INITIAL_COLOR = '#2c2c2c'
+const CANVAS_SIZE = 700
 
 // 캔버스는 두 개의 사이즈가 지정되어야 함-> css 사이즈, element 사이즈
 // 캔버스를 픽셀을 다룰 수 있는 element로서 만드는거니까 element에 width와 height를 지정해줘야함
 // 픽셀을 다루는 윈도우가 얼마나 큰지 canvas에게 알려줌
-canvas.width = 700
-canvas.height = 700
+canvas.width = CANVAS_SIZE
+canvas.height = CANVAS_SIZE
 
-ctx.strokeStyle = '#2c2c2c' //default 검정
-ctx.lineWidth = 2.5 //default 선 굵기
+ctx.fillStyle = 'white' // 이미지 저장할 때 default로 흰색 지정안해주면 배경 투명색으로 저장됨
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE) // x, y, w, h
+ctx.strokeStyle = INITIAL_COLOR // default 검정
+ctx.fillStyle = INITIAL_COLOR
+ctx.lineWidth = 2.5 // default 선 굵기
 
 let painting = false
+let filling = false
 
 function stopPainting(event) {
   painting = false
@@ -25,7 +35,6 @@ function startPainting(event) {
 // offsetX, offsetY: canvas 내에서 마우스 좌표
 // beginPath: context는 path(기본적인 선)를 가짐 -> 클릭할 때 path를 만듬(선의 시작점을 만듬)
 // 다시 클릭하면 시작점부터 클릭한 곳까지 선을 만듬
-
 function onMouseMove(event) {
   const x = event.offsetX
   const y = event.offsetY
@@ -38,10 +47,50 @@ function onMouseMove(event) {
   }
 }
 
+//Change Color
 function handleColorClick(event) {
   // console.log(event.target.style)
   const color = event.target.style.backgroundColor
   ctx.strokeStyle = color
+  ctx.fillStyle = color
+}
+
+//Brush Size
+function handleRangeChange(event) {
+  // console.log(event.target.value)
+  const size = event.target.value
+  ctx.lineWidth = size
+}
+
+function handleModeClick() {
+  if (filling === true) {
+    filling = false
+    mode.innerText = 'Fill'
+  } else {
+    filling = true
+    mode.innerText = 'Paint'
+    ctx.fillStyle = ctx.strokeStyle
+  }
+}
+
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE) // x, y, w, h
+  }
+}
+
+function handleCM(event) {
+  event.preventDefault() // 우클릭 방지
+}
+
+function handelSaveClick() {
+  const image = canvas.toDataURL() //default가 png
+  const link = document.createElement('a') // <a></a>
+  // href는 image(URL) 이 되어야하고 download는 image 이름이 되어야함
+  link.href = image // 링크를 저장 -> <a href="data:image/jpeg;base64,/9j/4A..."></a>
+  link.download = 'PaintJS[EXPORT]'
+  // console.log(link)
+  link.click()
 }
 
 if (canvas) {
@@ -52,8 +101,24 @@ if (canvas) {
   canvas.addEventListener('mouseup', stopPainting)
   // 캔버스에서 벗어나는 순간을 인지
   canvas.addEventListener('mouseleave', stopPainting)
+  // 캔버스를 클릭하는 순간을 인지
+  canvas.addEventListener('click', handleCanvasClick)
+  // 이미지 저장
+  canvas.addEventListener('contextmenu', handleCM)
 }
 
 Array.from(colors).forEach((color) =>
   color.addEventListener('click', handleColorClick),
 )
+
+if (range) {
+  range.addEventListener('input', handleRangeChange)
+}
+
+if (mode) {
+  mode.addEventListener('click', handleModeClick)
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener('click', handelSaveClick)
+}
